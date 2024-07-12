cbc3eef002df518b1c54b7e90d2dcf6ca846e5f2
Kirill <k187m18733@gmail.com>
2024-07-13 00:14:42 +0300

    feat: replaced the app class with a functional component

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,86 +1,88 @@
-import { Component, ReactNode } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchForm from './components/searchForm/searchForm.tsx';
 import SearchList from './components/searchList/searchList.tsx';
 import { getData, searchPerson } from './services/api';
 import { AppState } from './appTypes.ts';
 
-class App extends Component<object, AppState> {
-  state: AppState = {
+const App: React.FC = () => {
+  const [state, setState] = useState<AppState>({
     searchQuery: '',
     loading: true,
     results: [],
     error: false,
     errorMessage: '',
-  };
+  });
 
-  componentDidMount() {
+  useEffect(() => {
     const querySearch = localStorage.getItem('searchQuery');
     if (querySearch) {
-      this.setState({ searchQuery: querySearch });
-      this.getFetchData(querySearch);
+      setState((prevState) => ({ ...prevState, searchQuery: querySearch }));
+      getFetchData(querySearch);
     } else {
-      this.getFetchData('');
+      getFetchData('');
     }
-  }
+  }, []);
 
-  getFetchData = async (querySearch: string) => {
+  const getFetchData = async (querySearch: string) => {
     try {
       if (querySearch) {
         const person = await searchPerson(querySearch);
-        this.setState({ results: person, loading: false });
+        setState((prevState) => ({ ...prevState, results: person, loading: false }));
       } else {
         const data = await getData();
-        this.setState({ results: data.results, loading: false });
+        setState((prevState) => ({ ...prevState, results: data.results, loading: false }));
       }
     } catch (error) {
       console.error('Error fetching data');
-      this.setState({ loading: false });
+      setState((prevState) => ({ ...prevState, loading: false }));
     }
   };
 
-  handleSearch = () => {
-    if (this.state.searchQuery.trim() !== '') {
-      this.setState({ loading: true });
-      localStorage.setItem('searchQuery', this.state.searchQuery);
-      this.getFetchData(this.state.searchQuery);
+  const handleSearch = () => {
+    if (state.searchQuery.trim() !== '') {
+      setState((prevState) => ({ ...prevState, loading: true }));
+      localStorage.setItem('searchQuery', state.searchQuery);
+      getFetchData(state.searchQuery);
     } else {
       localStorage.removeItem('searchQuery');
-      this.getFetchData('');
+      getFetchData('');
     }
   };
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchQuery: event.target.value });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState((prevState) => ({ ...prevState, searchQuery: event.target.value }));
   };
 
-  getError = () => {
-    this.setState({ error: true, errorMessage: 'Opps! This is test error, just refresh page :)' });
+  const getError = () => {
+    setState((prevState) => ({
+      ...prevState,
+      error: true,
+      errorMessage: 'Opps! This is test error, just refresh page :)',
+    }));
   };
 
-  render(): ReactNode {
-    const { results, loading, searchQuery, errorMessage, error } = this.state;
+  const { results, loading, searchQuery, errorMessage, error } = state;
 
-    return (
-      <div className='container'>
-        <h1 className='title'>Star Wars characters</h1>
+  return (
+    <div className='container'>
+      <h1 className='title'>Star Wars characters</h1>
 
-        <SearchForm
-          searchQuery={searchQuery}
-          onChange={this.handleChange}
-          onSearch={this.handleSearch}
-          onErrorTest={this.getError}
-        />
-        <SearchList
-          searchQuery={searchQuery}
-          results={results}
-          loading={loading}
-          error={error}
-          errorMessage={errorMessage}
-        />
-      </div>
-    );
-  }
-}
+      <SearchForm
+        searchQuery={searchQuery}
+        onChange={handleChange}
+        onSearch={handleSearch}
+        onErrorTest={getError}
+      />
+      <SearchList
+        searchQuery={searchQuery}
+        results={results}
+        loading={loading}
+        error={error}
+        errorMessage={errorMessage}
+      />
+    </div>
+  );
+};
 
 export default App;
